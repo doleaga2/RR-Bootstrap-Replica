6a18e90615d7227251baa4b8582b4402921e2ac1
doleaga2 <oleaga.diana@gmail.com>
2023-02-07 18:20:08 -0500

    "failed attempt at nav bar"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
 
 function App() {
   return (
@@ -9,3 +12,15 @@ function App() {
 }
 
 export default App;
+// npm i - install node modules
+// npm install react-bootstrap bootstrap@[latest version]-install bootstrap library
+// import 'bootstrap/dist/css/bootstrap.min.css'- index.js helps components to work.
+// import Nav from 'react-bootstrap/Nav'
+// import Container from 'react-bootstrap/Container'- implement the navbar
+
+
+
+
+
+
+    
